1f4552f3c2470a5d940b2544c28883e45e47bbea
Sravan717 <sravangatti139@gmail.com>
2026-08-08 23:01:52 +0530

    Update vectors.v

--- verilog language/vectors/vectors.v
+++ verilog language/vectors/vectors.v
@@ -1,1 +1,12 @@
+// problem link .... https://hdlbits.01xz.net/wiki/Vector0
 
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); 
+    assign {o2,o1,o0} = vec[2:0];
+    assign outv = vec;
+
+endmodule
