eb15b838992daae6b109e79c4e414f738b0f6383
Sravan717 <sravangatti139@gmail.com>
2026-08-09 23:44:10 +0530

    Update 9_more_replication.v

--- verilog language/vectors/9_more_replication.v
+++ verilog language/vectors/9_more_replication.v
@@ -1,8 +1,11 @@
-// problem link   https://hdlbits.01xz.net/wiki/Vectorr
+// problem link   https://hdlbits.01xz.net/wiki/Vector5
 
-module top_module( 
-    input [7:0] in,
-    output [7:0] out
-);
-  assign {out[0],out[1],out[2],out[3],out[4],out[5],out[6],out[7]} = in;
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    assign out = {{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}}~^{5{a,b,c,d,e}};
+
 endmodule
