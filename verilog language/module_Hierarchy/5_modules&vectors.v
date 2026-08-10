c21c6626cfb8d1a08925140a1ee40d7885f90313
Sravan717 <sravangatti139@gmail.com>
2026-08-10 17:56:08 +0530

    Create 5_modules&vectors.v

---
+++ verilog language/module_Hierarchy/5_modules&vectors.v
@@ -0,0 +1,24 @@
+//  https://hdlbits.01xz.net/wiki/Module_shift8
+
+module top_module ( 
+    input clk, 
+    input [7:0] d, 
+    input [1:0] sel, 
+    output reg [7:0] q 
+);
+    wire [7:0] a,b,c;
+    my_dff8 inst1(clk,d,a);
+    my_dff8 inst2(clk,a,b);
+    my_dff8 inst3(clk,b,c);
+    always @(*) begin
+        case(sel)
+            2'b00: q = d;
+            2'b01: q = a;
+            2'b10: q = b;
+            2'b11: q = c;
+            default: q = 8'bx;
+        endcase
+    end
+    
+    
+endmodule
