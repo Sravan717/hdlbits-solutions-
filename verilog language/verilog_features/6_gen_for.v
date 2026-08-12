d203058e48dd12ce6ce495eeffde08954107eeac
Sravan717 <sravangatti139@gmail.com>
2026-08-12 15:42:25 +0530

    Create 6_gen_for.v

---
+++ verilog language/verilog_features/6_gen_for.v
@@ -0,0 +1,29 @@
+//  https://hdlbits.01xz.net/wiki/Adder100i
+
+
+module top_module( 
+    input [99:0] a, b,
+    input cin,
+    output [99:0] cout,
+    output [99:0] sum );
+    
+    wire [100:0] c;
+    assign c[0] = cin;
+    
+    generate
+        genvar i;
+        for(i=0;i<100;i++) begin: loop
+            add inst(a[i],b[i],c[i],sum[i],c[i+1]);
+            
+        end
+        
+    endgenerate  
+    
+    assign cout = c[100:1];
+    
+endmodule
+
+module add(input x,input y, input z ,output s, output ca);
+    
+    assign  { ca,s} = x+y+z;
+endmodule
