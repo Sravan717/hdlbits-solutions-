7ecc76892e0c15871d4fed0117e7730995eb5ca5
Sravan717 <sravangatti139@gmail.com>
2026-08-13 16:51:54 +0530

    Create 6_ringer.v

---
+++ combinational/basic gates/6_ringer.v
@@ -0,0 +1,15 @@
+//       https://hdlbits.01xz.net/wiki/Ringer
+
+
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
+    assign ringer = ~vibrate_mode&ring;
+    assign motor  = vibrate_mode&ring;
+
+endmodule
+
+
